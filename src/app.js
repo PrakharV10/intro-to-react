e1f99351fc3e2fc19ed9fe20866da87447b900ca
PrakharV10 <70899048+PrakharV10@users.noreply.github.com>
2021-01-31 23:15:56 +0530

    Added Eslint

--- src/app.js
+++ src/app.js
@@ -1,28 +1,37 @@
 import React from 'react';
 import { render } from 'react-dom';
-import child from './child';
+import Child from './child';
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me"),
-    React.createElement(child, {
-      name: "Chikara",
-      animal: "Cat",
-      breed: "Stray",
-    }),
+  // return React.createElement("div", {}, [
+  //   React.createElement("h1", {}, "Adopt Me"),
+  //   React.createElement(child, {
+  //     name: "Chikara",
+  //     animal: "Cat",
+  //     breed: "Stray",
+  //   }),
 
-    React.createElement(child, {
-      name: "Bagga",
-      animal: "Dog",
-      breed: "German Shephered",
-    }),
+  //   React.createElement(child, {
+  //     name: "Bagga",
+  //     animal: "Dog",
+  //     breed: "German Shephered",
+  //   }),
 
-    React.createElement(child, {
-      name: "Cookie",
-      animal: "Dog",
-      breed: "Unknown",
-    }),
-  ]);
+  //   React.createElement(child, {
+  //     name: "Cookie",
+  //     animal: "Dog",
+  //     breed: "Unknown",
+  //   }),
+  // ]);
+
+  return (
+    <div>
+      <h1>Adopt ME!</h1>
+      <Child name = "Chikara" animal = "Cat" breed = "Stray" />
+      <Child name = "Bagga" animal = "Dog" breed = "Derman Shephered" />
+      <Child name = "Cookie" animal = "Dog" breed = "Arpit" />
+    </div>
+  )
 };
 
-render(React.createElement(App), document.querySelector("#root"));
+render(<App />, document.querySelector("#root"));
--- src/child.js
+++ src/child.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
 export default function child ({ name, animal, breed }) {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("p", {}, breed),
-  ]);
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", {}, name),
+//     React.createElement("h2", {}, animal),
+//     React.createElement("p", {}, breed),
+//   ]);
+    return (
+        <div>
+            <h1>{name}</h1>
+            <h2>{animal}</h2>
+            <p>{breed}</p>
+        </div>
+    )
 };
